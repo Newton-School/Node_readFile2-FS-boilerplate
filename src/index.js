6830afd36c3e246a19e637b2f47b0c969fdf4760
Jatin-Mankhand <jatin.mankhand@newtonschool.co>
2023-04-25 11:30:26 +0530

    Done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs/promises");
+const fileName1 = "myfile1.txt";
+const fileName2 = "myfile2.txt";
+
+const reader = async (fileName1, fileName2) => {
+    //Write your code here
+    //Don't change function name
+
+   // read the content of myfile1 and myfile2
+  
+  // merge the content of the two files
+  
+};
+
+module.exports = reader;
+
